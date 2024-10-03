2c637986dc4b4d9a8313a0e213a92a15ab56532c
Ayush Mishra <130413472+AyushMishra1603@users.noreply.github.com>
2024-10-03 13:23:34 +0530

    Guess_a_letter.py

--- Guess.py
+++ Guess.py
@@ -1,1 +1,14 @@
-
+import random
+#Create a List
+word_list = ["ardvark", "baboon", "camel"]
+#Assign a random word from the list
+chosen_word = random.choice(word_list)
+#Take User's input and keep it in lower case
+guess = input("Guess a letter:").lower()
+#Run a loop and compare the word guessed by user with each letter of chosen word
+for letter in chosen_word:
+  if letter == guess:
+    print("Correct guess!")
+  else:
+    print("Incorrect guess!")
+print(chosen_word)
